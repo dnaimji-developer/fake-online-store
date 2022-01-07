73574131b7618fcd1bf6869e873ce16525bd7b4a
Dasri Naimji <DNaimji@users.noreply.github.com>
2022-01-06 21:04:05 -0500

    Add react-router-dom@5, enabling nagivation within app. Create following routes: home, delivery, payment, review.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { Router, Route } from "react-router-dom";
 
 import Products from "./Products";
 import Delivery from "./Delivery";
 import Payment from "./Payment";
 import Review from "./Review";
 
+import history from "../history";
+
 const App = () => {
 	return (
-		<div>
-			<Products />
-			<Delivery />
-			<Payment />
-			<Review />
-		</div>
+		<Router history={history}>
+			<div>
+				<Route path="/" exact component={Products} />
+				<Route path="/delivery" exact component={Delivery} />
+				<Route path="/payment" exact component={Payment} />
+				<Route path="/review" exact component={Review} />
+			</div>
+		</Router>
 	);
 };
 
